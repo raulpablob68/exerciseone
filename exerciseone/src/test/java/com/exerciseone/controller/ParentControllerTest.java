7cd7ca225c3180a9e7770598c80a0326a6107af8
rPablo <raul.pablo.b@gmail.com>
2019-05-17 20:12:18 -0500

    Séptimo commit.
    Agregada la prueba unitaria testGetOneFalse() que valida que el
    resultado es diferente a lo esperado (usa assertNotEquals). Cambiado el
    nombre de la prueba testGetOne() a testGetOneTrue().

--- exerciseone/src/test/java/com/exerciseone/controller/ParentControllerTest.java
+++ exerciseone/src/test/java/com/exerciseone/controller/ParentControllerTest.java
@@ -28,10 +28,17 @@ public class ParentControllerTest {
   @MockBean
   private ParentServiceImpl parentServiceImpl;
 
+  /**
+   * Instancia de Parent para realizar estas pruebas unitarias.
+   */
   Parent parentMock = new Parent(6, "M", "José", "José", "Jirones", "Otros");
 
+  /**
+   * Valida que el resultado ES igual a lo esperado. Emplea assertEquals.
+   * @throws Exception Control de excepciones.
+   */
   @Test
-  public void testGetOne() throws Exception {
+  public void testGetOneTrue() throws Exception {
     Mockito.when(parentServiceImpl.get(Mockito.anyInt())).thenReturn(parentMock);
 
     RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/1.0/parents/6")
@@ -44,6 +51,26 @@ public class ParentControllerTest {
         + "lastName:Jirones,otherParentDetails:Otros}";
 
     JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), true);
+  }
+  
+  /**
+   * Valida que el resultado NO es igual a lo esperado. Emplea assertNotEquals.
+   * @throws Exception Control de excepciones.
+   */
+  @Test
+  public void testGetOneFalse() throws Exception {
+    Mockito.when(parentServiceImpl.get(Mockito.anyInt())).thenReturn(parentMock);
+
+    RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/1.0/parents/6")
+        .accept(MediaType.APPLICATION_JSON);
+
+    MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+    System.out.println(result.getResponse());
+    String expected = "{parentId:7,gender:M,firstName:José,middleName:José,"
+        + "lastName:Jirones,otherParentDetails:Otros}";
+
+    JSONAssert.assertNotEquals(expected, result.getResponse().getContentAsString(), true);
   }
 
 }
